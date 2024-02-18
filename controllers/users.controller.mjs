4549e7703e4967ef54dcca26c719e6705c66b8a0
yurikahirata <yurikahirata0130@gmail.com>
2024-02-18 17:05:30 +0900

    feat: login functionality

--- controllers/users.controller.mjs
+++ controllers/users.controller.mjs
@@ -18,9 +18,40 @@ const usersController = {
       const hashedPassword = await bcrypt.hash(req.body.password, salt);
       user["hashedPassword"] = hashedPassword;
 
-      const newUser = await usersModel.create(user);
-      res.status(200).send(newUser);
+      try {
+        const newUser = await usersModel.create(user);
+        res.status(200).send(newUser);
+      } catch {
+        res.status(400).send("Username is taken");
+      }
 
+    }
+  },
+
+  login: async function (req, res) {
+    if (!validateUser(req.body))
+      res.status(400).send("Invalid input");
+    else {
+      const { username, password } = req.body;
+      const existingUser = await usersModel.getByUsername(username);
+
+      if (existingUser.length <= 0)
+        res.status(400).send("Incorrect username");
+      else {
+        const hashedPassword = existingUser[0]["hashedPassword"];
+        bcrypt.compare(password, hashedPassword, function (err, result) {
+          if (err) {
+            res.status(500).send("Error while comparing passwords");
+            return;
+          }
+
+          if (result) {
+            res.status(200).send("User authenticated");
+          } else {
+            res.status(401).send("Incorrect password");
+          }
+        });
+      }
     }
   }
 }
